46f0a510b65ce13e49308a62aa1b4a2dac71520b
plain30 <gnlf20@gmail.com>
2026-07-14 16:28:16 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const APP_CONFIG = {
+  version: '11.1.0',
+  // 현재 배포된 Google Apps Script 웹 앱 주소
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbw6qoyv-49RQDp7eaWqo7_y3xSPu_WfWEoWLOt-IFBj-_wBU2I_fXOAZk3xEBL3n-Xwg/exec',
+  adminLoginId: 'admin',
+  adminDisplayName: '한재식',
+  refreshIntervalMs: 15000
+};
