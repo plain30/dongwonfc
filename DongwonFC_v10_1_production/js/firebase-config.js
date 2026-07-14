c45a213e498df6754570167bc19f601e3afee04c
plain30 <gnlf20@gmail.com>
2026-07-14 15:07:55 +0900

    Add files via upload

---
+++ DongwonFC_v10_1_production/js/firebase-config.js
@@ -0,0 +1,18 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAji04iTLFqUniY0yaIddi5Phq3USaQ9XI",
+  authDomain: "project-782e9.firebaseapp.com",
+  projectId: "project-782e9",
+  storageBucket: "project-782e9.firebasestorage.app",
+  messagingSenderId: "334738569277",
+  appId: "1:334738569277:web:3c2fe10d43516f2c081254",
+  measurementId: "G-FNGYHQWY18"
+};
+
+export const APP_CONFIG = {
+  version: "10.1.0",
+  adminLoginId: "admin",
+  adminEmail: "admin@dongwonfc.app",
+  adminDisplayName: "한재식",
+  memberEmailDomain: "dongwonfc.app",
+  firebaseSdkVersion: "11.10.0"
+};
