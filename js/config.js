1ac0e7edd20c3a59adfb7049b5aaadb9d7f34fb4
plain30 <gnlf20@gmail.com>
2026-07-15 14:26:15 +0900

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-export const APP_CONFIG = {
-  version: '13.0.0',
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbw6qoyv-49RQDp7eaWqo7_y3xSPu_WfWEoWLOt-lFBi-_wBU2l_fXOAZk33xFBL3n-Xwg/exec',
-  adminLoginId: 'admin',
-  adminDisplayName: '한재식',
-  refreshIntervalMs: 15000,
-  jsonpTimeoutMs: 25000,
-  chunkSize: 4200
-};
